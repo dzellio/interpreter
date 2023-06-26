7182c1210f1dd406de9f9bcfc195b8d1055cbb72
Dzellio <jdcrinall@protonmail.com>
2023-06-26 13:25:20 +0100

    prefix operators

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -1,28 +1,152 @@
 package ast
 
+import (
+    "monkey/token"
+    "bytes"
+)
+
 type Node interface {
     TokenLiteral() string
+    String()       string
 }
 
-type Statement interfce {
+type Statement interface {
     Node
     statementNode()
 }
-
 type Expression interface {
     Node
     expressionNode()
 }
 
+
 type Program struct {
-    Statements []statement
+    Statements []Statement
 }
+
+type Identifier struct {
+    Token token.Token
+    Value string
+}
+
+type LetStatement struct {
+    Token token.Token
+    Name    *Identifier
+    Value   Expression
+}
+
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
 func (p *Program) TokenLiteral() string {
     if len(p.Statements) > 0 {
-        return p.Statements[0].TokenLiterlal()
+        return p.Statements[0].TokenLiteral()
     } else {
         return ""
     }
-
 }
+
+type ReturnStatement struct {
+    Token           token.Token
+    ReturnValue     Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+func (p *Program) String() string {
+    var out bytes.Buffer
+
+    for _, s := range p.Statements {
+        out.WriteString(s.String())
+    }
+
+    return out.String()
+}
+
+func (ls *LetStatement) String() string {
+    var out bytes.Buffer
+
+    out.WriteString(ls.TokenLiteral() + " ")
+    out.WriteString(ls.Name.String())
+    out.WriteString(" = ")
+
+    if ls.Value != nil {
+        out.WriteString(ls.Value.String())
+    }
+
+    out.WriteString(";")
+    return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+    var out bytes.Buffer
+
+    out.WriteString(rs.TokenLiteral() + " " )
+
+    if rs.ReturnValue != nil {
+        out.WriteString(rs.ReturnValue.String())
+    }
+
+    out.WriteString(";")
+    return out.String()
+}
+
+
+type ExpressionStatement struct {
+    Token   token.Token
+    Expression Expression
+}
+
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+
+func (es *ExpressionStatement) String() string {
+    if es.Expression != nil {
+        return es.Expression.String()
+    }
+
+    return ""
+}
+
+func (i *Identifier) String() string { return i.Value }
+
+
+
+type IntegerLiteral struct {
+    Token   token.Token
+    Value   int64
+}
+
+
+func (il *IntegerLiteral) expressionNode() {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string { return il.Token.Literal }
+
+type PrefixExpression struct {
+    Token       token.Token // prefix token e.g !
+    Operator    string
+    Right       Expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+    var out bytes.Buffer
+
+    out.WriteString("(")
+    out.WriteString(pe.Operator)
+    out.WriteString(pe.Right.String())
+    out.WriteString(")")
+
+    return out.String()
+}
+
+
---
+++ src/monkey/ast/ast_test.go
@@ -0,0 +1,31 @@
+package ast
+
+import (
+    "monkey/token"
+    "testing"
+)
+
+func TestString(t *testing.T) {
+    program := &Program{
+        Statements: []Statement{
+            &LetStatement{
+                Token:  token.Token{Type: token.LET, Literal: "let"},
+                Name: &Identifer{
+                    Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+                    Value: "myVar",
+                },
+                Value: &Identifer{
+                    Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+                    Value: "anotherVar"
+                },
+            },
+        },
+    }
+
+    if program.String() != "let myVar = anotherVar;" {
+        t.Errorf("program.String() wrong. got=%q", program.String())
+    }
+}
+
+
+stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
